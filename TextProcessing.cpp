f6a7e50cf4033ab8e497ffccf4bc9bcd4d13e463
Agne Chomiciute <agne.chomiciute@mif.stud.vu.lt>
2025-12-24 20:29:06 +0200

    pakeistas regex ir normalize funkcija

--- TextProcessing.cpp
+++ TextProcessing.cpp
@@ -5,7 +5,7 @@ std::string normalize(const std::string& rawWord) {
     std::string normalizedWord;
 
     for (unsigned char ch : rawWord) {
-        if (std::isalnum(static_cast<unsigned char>(ch))) {
+        if (std::isalpha(static_cast<unsigned char>(ch))) {
             normalizedWord.push_back(
                 static_cast<char>(std::tolower(static_cast<unsigned char>(ch)))
             );
